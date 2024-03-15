3d770ffed8eaf440bd669d01aadfe8fcad735b4d
Bryan C <bryan.cast@outlook.com>
2024-03-15 22:47:38 +1100

    String Utility Assignment 1. FINAL + Tidy up of the main() + Tidy up of the terminal printing... Clearer to read.

--- StringUtilityClass/StringUtilityClass.cpp
+++ StringUtilityClass/StringUtilityClass.cpp
@@ -1,21 +1,18 @@
-// StringUtilityClass.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 #include <iostream>
 #include <cstring>
 #include "String.h" // my header file
 
 using namespace std;
 
-
 int main()
 {
-    // ----- 1 -----
     // constructor with NO ARGUMENTS
     // class nameOfInstance;
     String string1;
     
-    // ----- 2 -----
+
     // covert a "string" to char array for input as variable(temp)
+    cout << "----- STRING USING CHAR[] -----" << endl;
     char temp[] = "written through char[]";
     String string2a(temp);
     cout << temp << endl;
@@ -23,11 +20,12 @@ int main()
     String string2b("written through overloaded");
     
 
-    // ----- 3 -----
+    cout << "----- STRING CLASS OVERLOADED -----" << endl;
     String string3(string2b);
     cout << string3.CStr() << endl;
 
-    // ----- 4 -----
+    
+    cout << "----- NON CONST CHARACTER AT FUNCTION -----" << endl;
     // doesn't include 'SPACE' as a character
     size_t index = 9;
     String string4aNonConst = "4.written Non Const";
@@ -36,27 +34,31 @@ int main()
     cout << "Character " << character << " is at position " << index << " of the string: " << string4aNonConst.CStr() << endl;
 
         // ALTERNATE WAY (const version)
+    cout << "----- CONST CHARACTER AT FUNCTION -----" << endl;
     const String constString("4.written as Const");
     const char& character1 = constString.CharacterAt(index);
     cout << "Character " << character1 << " is at position " << index << " of the string: " << constString.CStr() << endl;
     cout << "\n\n";
-    // ----- 5 -----
+
+
+    cout << "----- LENGTH FUNCTION -----" << endl;
     String stringL = "Hello";
     size_t StrLength = stringL.Length();
     cout << "The length of " << stringL.CStr() << " is: " << StrLength << endl;
     cout << "\n\n";
-    // ----- 6 -----
+    
+
+    cout << "----- EQUAL TO FUNCTION -----" << endl;
     String compare1 = "Hello there!";
     String compare2 = "Hello there!"; //change one of these strings to change outcome
     
     if (compare1.EqualTo(compare2) == 1)
         cout << "The strings are the same" << endl;
     else cout << "The strings are NOT the same" << endl;
-
     cout << "\n\n";
-    // ----- 7 -----
+    
 
-
+    cout << "----- APPEND FUNCTION -----" << endl;
     String str1("Hello ");
     String str2("World");
     cout << "Source string: " << str1.CStr() << endl;
@@ -64,44 +66,44 @@ int main()
     str1.Append(str2).Append("!");
     cout << str1.CStr() << endl;
 
+    cout << "----- PREPEND FUNCTION -----" << endl;
     String ogVar = "world";
     String PrependVar = ("Hello ");
     cout << "Source string: " << ogVar.CStr() << endl;
     cout << "String to Prepend: " << PrependVar.CStr() << endl;
     ogVar.Prepend(PrependVar);
     cout << ogVar.CStr() << endl;
-
     cout << "\n\n";
-    // ----- 8 -----
+    
+
+    cout << "----- CSTR FUNCTION -----" << endl;
     // this allows you to use the 'cout <<' on your string (as per below)
     String str3("can 'cout << string.CStr()' to console now!");
     cout << str3.CStr() << endl;
-
     // cout << str3 << endl; // <------This is a problem without it. Value is a char** (pointer to a pointer) not char* (just a pointer)
-
     cout << "\n\n";
-    // ----- 9 -----
+    
 
+    cout << "----- TO LOWER FUNCTION -----" << endl;
     String str4 = "TO LOWER CASE\n";
     str4.ToLower();
     cout << str4.CStr();
 
 
-    // ----- 10 -----
+    cout << "----- TO UPPER FUNCTION -----" << endl;
     String str5 = "to uppercase\n";
     str5.ToUpper();
     cout << str5.CStr();
-
-    
     cout << "\n\n";
-    // ----- 11 -----
+    
 
+    cout << "----- FIND FUNCTION FOR SUBSTRING -----" << endl;
     String str11("Hello, world");
     cout << "Total string: " << str11.CStr() << "\n";
     
     String substr11("world");
 
-    //call function to search for substr
+    
     size_t pos = str11.Find(substr11);
     if (pos != std::string::npos)
     {
@@ -112,8 +114,8 @@ int main()
         cout << "Substring not found." << endl;
     }
     
-    // ----- 12 -----
-
+    
+    cout << "----- FIND W/ INDEX FUNCTION -----" << endl;
     String str12("Hello, world");
     String substr12("llo");
     size_t posi = str11.Find(1, substr12); // .Find(index to start search, substr to find)
@@ -125,9 +127,10 @@ int main()
     {
         cout << "Substring not found." << endl;
     }
-    
     cout << "\n\n";
-    // ----- 13 -----
+    
+    
+    cout << "----- REPLACE FUNCTION -----" << endl;
 
     String str13("Hello, world");
     String substr13("everyone");
@@ -137,24 +140,25 @@ int main()
     str13.Replace("world", substr13);
     cout << "Final string (replaced): " << str13.CStr() << endl;
     cout << "\n\n";
-    // ----- 14 -----
+    
+
+    cout << "----- READ FROM CONSOLE FUNCTION -----" << endl;
     String fromConsole;
     fromConsole.ReadFromConsole();
     //cout << "String: " << fromConsole.CStr() << endl;
 
 
-    // ----- 15 -----
+    cout << "----- WRITE TO CONSOLE FUCNTION -----" << endl;
     cout << "write to console: ";
     fromConsole.WriteToConsole();
     cout << "\n\n";
 
-    // ----- 16 -----
+
+    cout << "----- OPERATOR == OVERLOAD -----" << endl;
     String equal1("Hello");
     String equal2("World");
     String equal3("Hello");
 
-    // Test == operator
-    cout << "---test == operator---" << endl;
     if (equal1 == equal2)
     {
         cout << "'equal1' is equal to 'equal2'" << endl;
@@ -169,10 +173,8 @@ int main()
         cout << "'equal1' is NOT equal to 'equal3'" << endl;
     }
 
-
-    // ----- 17 -----
-    // Test != operator
-    cout << "---test != operator---" << endl;
+    
+    cout << "----- OPERATOR != OVERLOAD -----" << endl;
     if (equal1 != equal2)
     {
         cout << "'equal1' is not equal to 'equal2'" << endl;
@@ -189,8 +191,9 @@ int main()
         cout << "'equal1' is equal to 'equal3'" << endl;
     }
     cout << "\n\n";
-    // ----- 18 -----
-    // test = operator
+       
+    
+    cout << "----- OPERATOR = OVERLOAD -----" << endl;
     String str18("Hello");
     String otherstr18("World");
 
@@ -199,21 +202,20 @@ int main()
     str18 = otherstr18;
     cout << "string result: " << str18.CStr() << endl;
 
-    // ----- 19 -----
-    // test [] operator
+    
+    cout << "----- OPERATOR [] OVERLOAD -----" << endl;
     String accessarray("Hello world");
     cout << "Original string: " << accessarray.CStr() << endl;
     accessarray[0] = 'J';
     cout << "Change array via element [0]: " << accessarray.CStr() << endl;
-    
-    // ----- 19 -----
-    //test const[] operator
+
+
+    cout << "----- CONST OPERATOR [] OVERLOAD -----" << endl;
     cout << accessarray.CStr() << endl;
     cout << "Accessed element at position [2] is: " << accessarray[2] << endl; // change [number] to access different elements
 
 
-    // ----- 20 -----
-    // test += operator
+    cout << "----- OPERATOR += OVERLOAD -----" << endl;
     String finalString("Hello ");
     String finalSubString("Cliff :)");
     cout << "Original string    " << finalString.CStr() << "\n" << "Substring:  " << finalSubString.CStr() << endl;
